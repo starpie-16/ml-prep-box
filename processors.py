f676b816e77ffd82faf4adcd6a9cab6eab1fc0b7
Oliver <n.hoangstarpie@gmail.com>
2026-02-12 12:40:10 +0700

    refactor: refactor scaling and imputation functions
    - Refactor apply_scaling and apply_imputation functions to return additional scaler and imputer objects along with the transformed data.

--- processors.py
+++ processors.py
@@ -1,14 +1,14 @@
 from sklearn.preprocessing import StandardScaler, RobustScaler, OneHotEncoder, LabelEncoder
 from sklearn.impute import SimpleImputer
+import pandas as pd
+
+
 
 def apply_scaling(series, method = 'standard'):
-  if method == 'robust_scale':
-    scaler = RobustScaler()
+  scaler = RobustScaler() if method == 'robust_scale' else StandardScaler()
+  scaled_data = scaler.fit_transform(series.values.reshape(-1, 1))
 
-  else: 
-    scaler = StandardScaler()
-
-  return scaler.fit_transform(series.values.reshape(-1, 1))
+  return scaled_data.flatten(), scaler
 
 
 
@@ -21,10 +21,12 @@ def apply_scaling(series, method = 'standard'):
 
 
 def apply_imputation(series, dtype):
-  strategy = 'median' if dtype = 'numeric' else 'most_frequent'
+  strategy = 'median' if dtype == 'numeric' else 'most_frequent'
   imputer = SimpleImputer(strategy = strategy)
 
-  return imupter.fit_transform(series.values.reshape(-1, 1))
+  imputed_data = imputer.fit_transform(series.values.reshape(-1, 1))
+
+  return imputed_data.flatten(), imputer
 
 
 
