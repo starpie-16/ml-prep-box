ecdc7b94928d5a8e3ce5da7e3dd5eab213e0e1bf
Oliver <n.hoangstarpie@gmail.com>
2026-02-18 22:34:53 +0700

    clean test_run.py

--- test_run.py
+++ test_run.py
@@ -1,9 +1,7 @@
-# test_run.py (nằm ngoài folder datawise)
 from datawise import toolbox
 import pandas as pd
 
 df = pd.read_csv('dataset/raw/titanic.csv')
 prep = toolbox()
-# Chạy thử thôi!
 prep.setup(df, target='Survived')
 prep.suggest()
